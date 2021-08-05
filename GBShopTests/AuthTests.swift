e287df711bef742e4a07f0e2c7a4b933a1e28748
Alexander Fomin <inzhener@outlook.com>
2021-08-05 22:16:56 +0300

    Дописал тесты по авторизации

--- GBShopTests/AuthTests.swift
+++ GBShopTests/AuthTests.swift
@@ -61,4 +61,91 @@ class AuthTests: XCTestCase {
         }
         wait(for: [expectation], timeout: 10)
     }
+    
+    func testLogout_withBaseURL_throwsNoErrors() throws {
+        let auth = Auth(errorParser: errorParser, sessionManager: commonSession)
+        auth.logout(id: 123) {[weak self] result in
+            switch result {
+            case .success(_):
+                XCTAssertTrue(true)
+            case .failure(_):
+                XCTFail()
+            }
+            self?.expectation.fulfill()
+        }
+        wait(for: [expectation], timeout: 10)
+    }
+    
+    func testLogout_withInvalidURL_throwsErrors() throws {
+        let baseUrl = "https://failUrl"
+        let auth = Auth(baseURL: baseUrl, errorParser: errorParser, sessionManager: commonSession)
+        auth.logout(id: 123) {[weak self] result in
+            switch result {
+            case .success(_):
+                XCTFail("Expected error")
+            case .failure(_):
+                XCTAssertTrue(true)
+            }
+            self?.expectation.fulfill()
+        }
+        wait(for: [expectation], timeout: 10)
+    }
+    
+    func testRegisterUser_withBaseURL_throwsNoErrors() throws {
+        let auth = Auth(errorParser: errorParser, sessionManager: commonSession)
+        auth.registerUser(id: 123, userName: "Somebody", password: "mypassword", email: "some@some.ru", gender: UserGender.male, creditCard: "9872389-2424-234224-234", bio: "This is good! I think I will switch to another language") {[weak self] result in
+            switch result {
+            case .success(_):
+                XCTAssertTrue(true)
+            case .failure(_):
+                XCTFail()
+            }
+            self?.expectation.fulfill()
+        }
+        wait(for: [expectation], timeout: 10)
+    }
+    
+    func testRegisterUser_withInvalidURL_throwsErrors() throws {
+        let baseUrl = "https://failUrl"
+        let auth = Auth(baseURL: baseUrl, errorParser: errorParser, sessionManager: commonSession)
+        auth.registerUser(id: 123, userName: "Somebody", password: "mypassword", email: "some@some.ru", gender: UserGender.male, creditCard: "9872389-2424-234224-234", bio: "This is good! I think I will switch to another language") {[weak self] result in
+            switch result {
+            case .success(_):
+                XCTFail("Expected error")
+            case .failure(_):
+                XCTAssertTrue(true)
+            }
+            self?.expectation.fulfill()
+        }
+        wait(for: [expectation], timeout: 10)
+    }
+    
+    func testChangeUserData_withBaseURL_throwsNoErrors() throws {
+        let auth = Auth(errorParser: errorParser, sessionManager: commonSession)
+        auth.changeUserData(id: 123, userName: "Somebody", password: "mypassword", email: "some@some.ru", gender: UserGender.male, creditCard: "9872389-2424-234224-234", bio: "This is good! I think I will switch to another language") {[weak self] result in
+            switch result {
+            case .success(_):
+                XCTAssertTrue(true)
+            case .failure(_):
+                XCTFail()
+            }
+            self?.expectation.fulfill()
+        }
+        wait(for: [expectation], timeout: 10)
+    }
+    
+    func testChangeUserData_withInvalidURL_throwsErrors() throws {
+        let baseUrl = "https://failUrl"
+        let auth = Auth(baseURL: baseUrl, errorParser: errorParser, sessionManager: commonSession)
+        auth.changeUserData(id: 123, userName: "Somebody", password: "mypassword", email: "some@some.ru", gender: UserGender.male, creditCard: "9872389-2424-234224-234", bio: "This is good! I think I will switch to another language") {[weak self] result in
+            switch result {
+            case .success(_):
+                XCTFail("Expected error")
+            case .failure(_):
+                XCTAssertTrue(true)
+            }
+            self?.expectation.fulfill()
+        }
+        wait(for: [expectation], timeout: 10)
+    }
 }
