3ee38257a996032b49486785e0b93e001ace5020
Alexander Fomin <inzhener@outlook.com>
2021-09-01 22:05:06 +0300

    Тулбар для клавиатуры

--- GBShop/App/Flows/Auth/Controllers/LoginViewController.swift
+++ GBShop/App/Flows/Auth/Controllers/LoginViewController.swift
@@ -22,6 +22,7 @@ class LoginViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        addKeyboardToolbar()
         
         let hideKeyboardGesture = UITapGestureRecognizer(target: self, action: #selector(hideKeyboard))
         self.loginView.scrollView.addGestureRecognizer(hideKeyboardGesture)
@@ -35,6 +36,16 @@ class LoginViewController: UIViewController {
         }
     
     // MARK: - Keyboard functions
+    private func addKeyboardToolbar() {
+        let keyboardToolbar = UIToolbar()
+            keyboardToolbar.sizeToFit()
+            let flexBarButton = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: nil, action: nil)
+        let doneBarButton = UIBarButtonItem(title: "Готово", style: .plain, target: self, action: #selector(hideKeyboard))
+            keyboardToolbar.items = [flexBarButton, doneBarButton]
+        self.loginView.loginTextField.inputAccessoryView = keyboardToolbar
+        self.loginView.passwordTextField.inputAccessoryView = keyboardToolbar
+    }
+    
     @objc func hideKeyboard() {
         self.loginView.endEditing(true)
     }
@@ -45,6 +56,9 @@ class LoginViewController: UIViewController {
             let endRect = view.convert((endValue as AnyObject).cgRectValue, from: view.window)
             let keyboardOverlap = endRect.height + self.view.safeAreaInsets.bottom + self.view.safeAreaInsets.top
             self.loginView.scrollView.contentInset.bottom = keyboardOverlap
+            self.loginView.scrollView.verticalScrollIndicatorInsets.bottom = keyboardOverlap
+            let bottomOffset = CGPoint(x: 0, y: self.loginView.scrollView.contentSize.height - self.loginView.scrollView.bounds.height + keyboardOverlap)
+            self.loginView.scrollView.setContentOffset(bottomOffset, animated: true)
         }
     }
     
