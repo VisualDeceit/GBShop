fc309692cb26cc7edc7e9f7ba5b42254f6c6804f
Alexander Fomin <inzhener@outlook.com>
2021-09-08 21:42:27 +0300

    Отображение каталога

--- GBShop/App/Flows/Goods/Controllers/CatalogViewController.swift
+++ GBShop/App/Flows/Goods/Controllers/CatalogViewController.swift
@@ -8,6 +8,19 @@
 import UIKit
 
 class CatalogViewController: UIViewController {
+    private var catalogView: CatalogView {
+        // swiftlint:disable force_cast
+        self.view as! CatalogView
+        // swiftlint:enable force_cast
+    }
+    
+    let requestFactory = RequestFactory()
+    var goods: GoodsRequestFactory!
+    var products = [Product]()
+    
+    override func loadView() {
+        self.view = CatalogView()
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -18,6 +31,43 @@ class CatalogViewController: UIViewController {
         navigationController?.navigationBar.largeTitleTextAttributes = [
             .foregroundColor: UIColor.cinnabar,
             .font: UIFontMetrics.default.scaledFont(for: UIFont.captionParangon50)
-        ] 
+        ]
+        
+        catalogView.tableView.dataSource = self
+        
+        goods = requestFactory.makeGoodsRequestFatory()
+        getCatalog()
+    }
+    
+    private func getCatalog() {
+        goods.getCatalogData(page: 1, category: 1) { [weak self] result in
+            switch result {
+            case .success(let catalog):
+                self?.products = catalog
+                self?.catalogView.tableView.reloadData()
+            case .failure(let error):
+                print(error.localizedDescription)
+            }
+        }
+    }
+}
+
+extension CatalogViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        products.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        var dequeuedCell = tableView.dequeueReusableCell(withIdentifier: "Cell")
+        if dequeuedCell == nil {
+            dequeuedCell = UITableViewCell(style: UITableViewCell.CellStyle.subtitle, reuseIdentifier: "Cell")
+        }
+        
+        guard let cell = dequeuedCell else {
+            return UITableViewCell()
+        }
+        cell.textLabel?.text = products[indexPath.row].name
+        cell.detailTextLabel?.text = "\(products[indexPath.row].price) руб."
+        return cell
     }
 }
---
+++ GBShop/App/Flows/Goods/Views/CatalogView.swift
@@ -0,0 +1,33 @@
+//
+//  CatalogView.swift
+//  GBShop
+//
+//  Created by Alexander Fomin on 08.09.2021.
+//
+
+import UIKit
+
+class CatalogView: UIView {
+    private(set) lazy var tableView = UITableView()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configureUI()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func configureUI() {
+        self.addSubview(tableView)
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            tableView.topAnchor.constraint(equalTo: self.topAnchor),
+            tableView.leadingAnchor.constraint(equalTo: self.leadingAnchor),
+            tableView.trailingAnchor.constraint(equalTo: self.trailingAnchor),
+            tableView.bottomAnchor.constraint(equalTo: self.bottomAnchor)
+        ])
+    }
+}
